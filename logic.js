97dec8679417d661b75b9d68017798830d165484
Carlos Carvajal <ing.carlosj@gmail.com>
2018-11-17 13:19:45 -0500

    Update the list of annoying classes

--- logic.js
+++ logic.js
@@ -18,9 +18,31 @@
 
 				elem = document.getElementsByClassName("cookies-acceptance")[0];
 				removeElement(elem);
+
+                elem = document.getElementsByClassName("adroll-block")[0];
+                removeElement(elem);
+
+                elem = document.getElementsByClassName("google_image_div")[0];
+                removeElement(elem);
 	
 				elem = document.getElementsByClassName("cc_banner-wrapper")[0];
 				removeElement(elem);
+
+                elem = document.getElementsByClassName("evidon-banner")[0];
+                removeElement(elem);
+
+                elem = document.getElementsByClassName("fe-blogs__top-ad-wrapper")[0];
+                removeElement(elem);
+
+                elem = document.getElementsByClassName("ribbon__clickable-banner")[0];
+                removeElement(elem);
+
+                elem = document.getElementsByClassName("ribbon__banner")[0];
+                removeElement(elem);
+
+                elem = document.getElementsByClassName("inhouse__subscription-ribbon")[0];
+                removeElement(elem);
+                
              	` });
      });
  }
